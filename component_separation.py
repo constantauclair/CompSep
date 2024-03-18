644942ec62d993c634f43ae1ba6242e381ce90d6
constantauclair <constantauclair1998@gmail.com>
2024-03-18 15:21:22 +0100

    add_comments

--- component_separation.py
+++ component_separation.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
             coeffs_target = torch.cat((torch.unsqueeze(torch.real(coeffs),dim=0),torch.unsqueeze(torch.imag(coeffs),dim=0)))
         if style == 'JM':
             coeffs_target = torch.cat((torch.unsqueeze(torch.real(coeffs)-bias[0],dim=0),torch.unsqueeze(torch.imag(coeffs)-bias[1],dim=0)))
-        mask = compute_mask(1, s_tilde0, wph_op, wph_model, pbc, device) # Mask computation
+        mask = compute_mask(1, s_tilde0, std, wph_op, wph_model, pbc, device) # Mask computation
         print('Stuff computed !')
         print('Beginning optimization...')
         result = opt.minimize(objective, s_tilde0.cpu().ravel(), args=(device, style, coeffs_target, std, mask, wph_op, noise, pbc, N, Mn), method=method, jac=True, tol=None, options={"maxiter": iter_per_step, "gtol": 1e-14, "ftol": 1e-14, "maxcor": 20})
